f9030dfb03366206b6c3f61aa2430084c20d4d25
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-07-12 17:05:06 +0700

    create search page

--- src/main/java/org/example/j2ee/HelloServlet.java
+++ src/main/java/org/example/j2ee/HelloServlet.java
@@ -3,25 +3,27 @@ package org.example.j2ee;
 import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lombok.SneakyThrows;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
-    public void init() {
-        message = "Hello World!";
+
+
+    @SneakyThrows
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/jsp/search.jsp").forward(request,response);
     }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
-
     public void destroy() {
     }
 }
